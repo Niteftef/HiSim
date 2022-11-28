a534d73d47a8f25af247b4a0a1a22d5dbcb13b3b
k-rieck <k.rieck@fz-juelich.de>
2022-11-28 15:31:03 +0100

    some changes on building thermal conductance ventilation

--- hisim/components/building.py
+++ hisim/components/building.py
@@ -1115,37 +1115,41 @@ class Building(dynamic_component.DynamicComponent):
     ):
         """Based on the RC_BuildingSimulator project @[rc_buildingsimulator-jayathissa] (** Check header)."""
         # Long from for H_ve_adj: Ventilation
+        # Determine the ventilation conductance
         if self.ven_method == "RC_BuildingSimulator":
             """Based on the RC_BuildingSimulator project @[rc_buildingsimulator-jayathissa] (** Check header)."""
             air_changes_per_hour_through_ventilation = 1.5
             air_changes_per_hour_through_infiltration = 0.5
-            ventilation_efficiency = 0.6
-            # Determine the ventilation conductance
+            # The efficiency of the heat recovery system for ventilation. Set to 0 if there is no heat recovery
+            ventilation_efficiency_of_the_heat_recovery_system = 0.6
+            heat_capacity_of_air_per_volume_in_joule_per_m3_per_kelvin = 1200
+
             total_air_changes_per_hour = (
                 air_changes_per_hour_through_infiltration
                 + air_changes_per_hour_through_ventilation
             )
-            # temperature adjustment factor taking ventilation and infiltration
-            # [ISO: E -27]
-            b_ek = (
+
+            # Fraction of the considered air flow that goes through the heat recovery system
+            fraction_of_air_flow_through_heat_recovery_system = air_changes_per_hour_through_ventilation / total_air_changes_per_hour
+
+            # Temperature adjustment factor taking ventilation and infiltration
+            # [ISO: 9.3.2.8]
+            temperature_adjustment_factor_for_air_flow_from_a_heat_recovery_system = (
                 1
-                - (
-                    air_changes_per_hour_through_ventilation
-                    / total_air_changes_per_hour
-                )
-                * ventilation_efficiency
+                - fraction_of_air_flow_through_heat_recovery_system
+                * ventilation_efficiency_of_the_heat_recovery_system
             )
+            # [ISO: 9.3.1]
             self.thermal_conductance_by_ventilation_in_watt_per_kelvin = float(
-                1200
-                * b_ek
+                heat_capacity_of_air_per_volume_in_joule_per_m3_per_kelvin
+                * temperature_adjustment_factor_for_air_flow_from_a_heat_recovery_system
                 * self.room_volume_in_m3
                 * (total_air_changes_per_hour / 3600)
-            )  # Conductance through ventilation [W/M]
+            )
         elif self.ven_method == "EPISCOPE":
-            # cp = 0.00028378 * 1E3  # [Wh/m3K]
-            c_p = 0.34
+            heat_capacity_of_air_per_volume_in_watt_hour_per_m3_per_kelvin = 0.34
             self.thermal_conductance_by_ventilation_in_watt_per_kelvin = (
-                c_p
+                heat_capacity_of_air_per_volume_in_watt_hour_per_m3_per_kelvin
                 * float(
                     self.buildingdata["n_air_use"]
                     + self.buildingdata["n_air_infiltration"]
