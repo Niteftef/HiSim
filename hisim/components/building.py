5fa19d85c2456fe169ba760773165ea3256894e7
k-rieck <k.rieck@fz-juelich.de>
2022-11-29 16:13:20 +0100

    1759 calc_solar_gains ausgeklammert weil nicht benutzt

--- hisim/components/building.py
+++ hisim/components/building.py
@@ -1372,10 +1372,10 @@ class Building(dynamic_component.DynamicComponent):
             "North",
             "West",
         ]
-        f_w = self.buildingdata["F_w"].values[0]
-        f_f = self.buildingdata["F_f"].values[0]
-        f_sh_vertical = self.buildingdata["F_sh_vert"].values[0]
-        g_gln = self.buildingdata["g_gl_n"].values[0]
+        reduction_factor_for_non_perpedicular_radiation = self.buildingdata["F_w"].values[0]
+        reduction_factor_for_frame_area_fraction_of_window = self.buildingdata["F_f"].values[0]
+        reduction_factor_for_external_vertical_shading = self.buildingdata["F_sh_vert"].values[0]
+        total_solar_energy_transmittance_for_perpedicular_radiation = self.buildingdata["g_gl_n"].values[0]
         for windows_direction in windows_directions:
             area = float(self.buildingdata["A_Window_" + windows_direction])
             if area != 0.0:
@@ -1383,10 +1383,10 @@ class Building(dynamic_component.DynamicComponent):
                     Window(
                         azimuth_tilt=windows_angles[windows_direction],
                         area=area,
-                        frame_area_fraction_reduction_factor=f_f,
-                        glass_solar_transmittance=g_gln,
-                        nonperpendicular_reduction_factor=f_w,
-                        external_shading_vertical_reduction_factor=f_sh_vertical,
+                        frame_area_fraction_reduction_factor=reduction_factor_for_frame_area_fraction_of_window,
+                        glass_solar_transmittance=total_solar_energy_transmittance_for_perpedicular_radiation,
+                        nonperpendicular_reduction_factor=reduction_factor_for_non_perpedicular_radiation,
+                        external_shading_vertical_reduction_factor=reduction_factor_for_external_vertical_shading,
                     )
                 )
                 self.windows_area += area
@@ -1756,80 +1756,80 @@ class Window:
 
         self.reduction_factor_with_area = self.reduction_factor * area
 
-    # @cached(cache=LRUCache(maxsize=5))
-    # @lru_cache
-    def calc_solar_gains(
-        self,
-        sun_azimuth,
-        direct_normal_irradiance,
-        direct_horizontal_irradiance,
-        global_horizontal_irradiance,
-        direct_normal_irradiance_extra,
-        apparent_zenith,
-    ):
-        """Calculates the Solar Gains in the building zone through the set Window.
+    # # @cached(cache=LRUCache(maxsize=5))
+    # # @lru_cache
+    # def calc_solar_gains(
+    #     self,
+    #     sun_azimuth,
+    #     direct_normal_irradiance,
+    #     direct_horizontal_irradiance,
+    #     global_horizontal_irradiance,
+    #     direct_normal_irradiance_extra,
+    #     apparent_zenith,
+    # ):
+    #     """Calculates the Solar Gains in the building zone through the set Window.
 
-        :param sun_altitude: Altitude Angle of the Sun in Degrees
-        :type sun_altitude: float
-        :param sun_azimuth: Azimuth angle of the sun in degrees
-        :type sun_azimuth: float
-        :param normal_direct_radiation: Normal Direct Radiation from weather file
-        :type normal_direct_radiation: float
-        :param horizontal_diffuse_radiation: Horizontal Diffuse Radiation from weather file
-        :type horizontal_diffuse_radiation: float
-        :return: self.incident_solar, Incident Solar Radiation on window
-        :return: self.solar_gains - Solar gains in building after transmitting through the window
-        :rtype: float
-        """
-        albedo = 0.4
-        # automatic pd time series in future pvlib version
-        # calculate airmass
-        airmass = pvlib.atmosphere.get_relative_airmass(apparent_zenith)
-        # use perez model to calculate the plane of array diffuse sky radiation
-        poa_sky_diffuse = pvlib.irradiance.perez(
-            self.altitude_tilt,
-            self.azimuth_tilt,
-            direct_horizontal_irradiance,
-            np.float64(direct_normal_irradiance),
-            direct_normal_irradiance_extra,
-            apparent_zenith,
-            sun_azimuth,
-            airmass,
-        )
-        # calculate ground diffuse with specified albedo
-        poa_ground_diffuse = pvlib.irradiance.get_ground_diffuse(
-            self.altitude_tilt,
-            global_horizontal_irradiance,
-            albedo=albedo,
-        )
-        # calculate angle of incidence
-        aoi = pvlib.irradiance.aoi(
-            self.altitude_tilt,
-            self.azimuth_tilt,
-            apparent_zenith,
-            sun_azimuth,
-        )
-        # calculate plane of array irradiance
-        poa_irrad = pvlib.irradiance.poa_components(
-            aoi,
-            np.float64(direct_normal_irradiance),
-            poa_sky_diffuse,
-            poa_ground_diffuse,
-        )
+    #     :param sun_altitude: Altitude Angle of the Sun in Degrees
+    #     :type sun_altitude: float
+    #     :param sun_azimuth: Azimuth angle of the sun in degrees
+    #     :type sun_azimuth: float
+    #     :param normal_direct_radiation: Normal Direct Radiation from weather file
+    #     :type normal_direct_radiation: float
+    #     :param horizontal_diffuse_radiation: Horizontal Diffuse Radiation from weather file
+    #     :type horizontal_diffuse_radiation: float
+    #     :return: self.incident_solar, Incident Solar Radiation on window
+    #     :return: self.solar_gains - Solar gains in building after transmitting through the window
+    #     :rtype: float
+    #     """
+    #     albedo = 0.4
+    #     # automatic pd time series in future pvlib version
+    #     # calculate airmass
+    #     airmass = pvlib.atmosphere.get_relative_airmass(apparent_zenith)
+    #     # use perez model to calculate the plane of array diffuse sky radiation
+    #     poa_sky_diffuse = pvlib.irradiance.perez(
+    #         self.altitude_tilt,
+    #         self.azimuth_tilt,
+    #         direct_horizontal_irradiance,
+    #         np.float64(direct_normal_irradiance),
+    #         direct_normal_irradiance_extra,
+    #         apparent_zenith,
+    #         sun_azimuth,
+    #         airmass,
+    #     )
+    #     # calculate ground diffuse with specified albedo
+    #     poa_ground_diffuse = pvlib.irradiance.get_ground_diffuse(
+    #         self.altitude_tilt,
+    #         global_horizontal_irradiance,
+    #         albedo=albedo,
+    #     )
+    #     # calculate angle of incidence
+    #     aoi = pvlib.irradiance.aoi(
+    #         self.altitude_tilt,
+    #         self.azimuth_tilt,
+    #         apparent_zenith,
+    #         sun_azimuth,
+    #     )
+    #     # calculate plane of array irradiance
+    #     poa_irrad = pvlib.irradiance.poa_components(
+    #         aoi,
+    #         np.float64(direct_normal_irradiance),
+    #         poa_sky_diffuse,
+    #         poa_ground_diffuse,
+    #     )
 
-        if math.isnan(poa_irrad["poa_direct"]):
-            self.incident_solar = 0
-        else:
-            self.incident_solar = (poa_irrad["poa_direct"]) * self.area
+    #     if math.isnan(poa_irrad["poa_direct"]):
+    #         self.incident_solar = 0
+    #     else:
+    #         self.incident_solar = (poa_irrad["poa_direct"]) * self.area
 
-        solar_gains = (
-            self.incident_solar
-            * self.glass_solar_transmittance
-            * self.nonperpendicular_reduction_factor
-            * self.external_shading_vertical_reduction_factor
-            * (1 - self.frame_area_fraction_reduction_factor)
-        )
-        return solar_gains
+    #     solar_gains = (
+    #         self.incident_solar
+    #         * self.glass_solar_transmittance
+    #         * self.nonperpendicular_reduction_factor
+    #         * self.external_shading_vertical_reduction_factor
+    #         * (1 - self.frame_area_fraction_reduction_factor)
+    #     )
+    #     return solar_gains
 
     def calc_direct_solar_factor(
         self,
